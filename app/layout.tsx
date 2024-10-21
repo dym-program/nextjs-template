a31521828a2fd1755ea90a7fd6aa82a009cec6cf
ugpu <fashiorg@gmail.com>
2024-10-21 21:39:34 +0800

    keep submit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body> 
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
