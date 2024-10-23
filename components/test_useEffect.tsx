6bf69ce3fedb35b9801004543cccc9c3be8d3620
ugpu <fashiorg@gmail.com>
2024-10-23 16:16:57 +0800

    add test

---
+++ components/test_useEffect.tsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>seconds: {seconds}</div>;
+}
+
+export default Timer;
+
